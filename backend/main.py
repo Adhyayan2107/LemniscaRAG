c2af6668480bdfcc79d1f0a60938c1699e167bf7
adhyayan <adhyayan2107@gmail.com>
2026-02-21 17:07:50 +0530

    relative path change for vectorDB

--- backend/main.py
+++ backend/main.py
@@ -14,19 +14,20 @@ from backend.text_chunking.text_splitter_langchain import split_documents
 from backend.text_chunking.loadingdata import load_documents
 from backend.vector_store.vector_store import save_chunks, load_chunks
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-VECTOR_STORE_PATH = os.path.join(BASE_DIR, "vector_store", "vector_store.pkl")
-DOCS_PATH = os.path.join(BASE_DIR, "..", "clearpath_docs")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+VECTOR_STORE_PATH = os.path.join(BASE_DIR, "vector_store.pkl")
+DOCS_PATH = os.path.join(BASE_DIR, "clearpath_docs")
+
+print("Vector path:", VECTOR_STORE_PATH)
+print("Vector exists:", os.path.exists(VECTOR_STORE_PATH))
 
 if os.path.exists(VECTOR_STORE_PATH):
-    chunks = load_chunks()
+    chunks = load_chunks(VECTOR_STORE_PATH)
     print("Loaded saved embeddings")
+
 else:
-    documents = load_documents(DOCS_PATH)
-    chunks = split_documents(documents)
-    chunks = embed_chunks(chunks)
-    save_chunks(chunks)
-    print("Created embeddings")
+    print("Vector store not found!")
+    raise Exception("vector_store.pkl missing")
 
 class QueryRequest(BaseModel):
     question: str
