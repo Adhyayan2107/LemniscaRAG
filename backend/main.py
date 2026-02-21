80959be52d0cd27fc864574a9276ee347e4d2255
adhyayan <adhyayan2107@gmail.com>
2026-02-21 17:16:14 +0530

    made changes to deploy on render

--- backend/main.py
+++ backend/main.py
@@ -14,28 +14,50 @@ from backend.text_chunking.text_splitter_langchain import split_documents
 from backend.text_chunking.loadingdata import load_documents
 from backend.vector_store.vector_store import save_chunks, load_chunks
 
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 VECTOR_STORE_PATH = os.path.join(BASE_DIR, "vector_store.pkl")
 DOCS_PATH = os.path.join(BASE_DIR, "clearpath_docs")
+chunks = None
 
-print("Vector path:", VECTOR_STORE_PATH)
-print("Vector exists:", os.path.exists(VECTOR_STORE_PATH))
 
-if os.path.exists(VECTOR_STORE_PATH):
-    chunks = load_chunks(VECTOR_STORE_PATH)
-    print("Loaded saved embeddings")
+@app.on_event("startup")
+def load_vectors():
 
-else:
-    print("Vector store not found!")
-    raise Exception("vector_store.pkl missing")
+    global chunks
+
+    print("Starting application...")
+    print("Vector path:", VECTOR_STORE_PATH)
+    print("Vector exists:", os.path.exists(VECTOR_STORE_PATH))
+
+    if os.path.exists(VECTOR_STORE_PATH):
+        chunks = load_chunks(VECTOR_STORE_PATH)
+        print("Loaded saved embeddings")
+
+    else:
+
+        print("Vector store missing — creating embeddings")
+        documents = load_documents(DOCS_PATH)
+        chunks_split = split_documents(documents)
+        chunks_embedded = embed_chunks(chunks_split)
+        save_chunks(chunks_embedded, VECTOR_STORE_PATH)
+        chunks = chunks_embedded
+
+        print("Created embeddings")
+
 
 class QueryRequest(BaseModel):
     question: str
     conversation_id: str | None = None
 
+
 @app.post("/query")
 def query_endpoint(request: QueryRequest):
+
     conversation_id = request.conversation_id or f"conv_{uuid.uuid4().hex[:8]}"
+
     result = process_query(request.question, chunks)
+
     result["conversation_id"] = conversation_id
+
     return result
